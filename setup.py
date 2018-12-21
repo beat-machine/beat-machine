5936bca72e6eb5649f14df44f10a0df8eec095da
dhsavell <dhsavell05@gmail.com>
2018-12-21 13:38:12 -0500

    Update setup.py to properly include module

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/dhsavell/beat-machine'
 EMAIL = 'dhsavell05@gmail.com'
 AUTHOR = 'dhsavell'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.0'
+VERSION = '0.0.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -100,7 +100,7 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    py_modules=['beatmachine'],
+    packages=find_packages(exclude=('test',)),
 
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
