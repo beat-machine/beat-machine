53d7d12b8e39aff0957ac25da495e2af88432f89
dhsavell <dhsavell05@gmail.com>
2018-12-24 18:41:56 -0500

    Bump version: 0.0.2 → 1.0.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/dhsavell/beat-machine'
 EMAIL = 'dhsavell05@gmail.com'
 AUTHOR = 'dhsavell'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.2'
+VERSION = '1.0.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
