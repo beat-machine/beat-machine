2ae978fdd20c36133e4a7d71dfd3c3d21c9d3630
dhsavell <dhsavell05@gmail.com>
2018-12-24 15:59:03 -0500

    Remove BPM detection and clean up processor.py

--- beatmachine/processor.py
+++ beatmachine/processor.py
@@ -1,14 +1,12 @@
-from pydub.silence import detect_nonsilent
-
-
-def split_into_beats(audio, beat_size_ms):
+def beats_of(audio, bpm):
     """
     Splits a given AudioSegment into a list of beats and their indices. Each beat is represented as a Beat object.
 
     :param audio: The AudioSegment to split into beats.
-    :param beat_size_ms: The length of one beat (in milliseconds).
+    :param bpm: BPM of the given audio.
     :return: A generator yielding each beat and its index.
     """
+    beat_size_ms = 60_000 // bpm
     for beat_start_ms in range(0, len(audio), beat_size_ms):
         yield audio[beat_start_ms:beat_start_ms + beat_size_ms]
 
@@ -31,34 +29,6 @@ def remove_leading_silence(audio, check_size_ms=10, threshold=-60):
     return audio[trim_amount:]
 
 
-def estimate_bpm(audio):
-    """
-    Estimates the BPM of a given audio segment.
-
-    Adapted from: https://gist.github.com/jiaaro/faa96fabd252b8552066
-
-    :param audio: The AudioSegment to estimate the BPM of.
-    :return: An approximate BPM of the given AudioSegment.
-    """
-    bass = audio.low_pass_filter(120)
-    beat_loudness = bass.dBFS
-
-    minimum_silence = int(60_000 / 240)
-    nonsilent_times = detect_nonsilent(bass, minimum_silence, beat_loudness)
-
-    spaces_between_beats = []
-    last_t = nonsilent_times[0][0]
-
-    for peak_start, _ in nonsilent_times[1:]:
-        spaces_between_beats.append(peak_start - last_t)
-        last_t = peak_start
-
-    spaces_between_beats = sorted(spaces_between_beats)
-    space = spaces_between_beats[len(spaces_between_beats) // 2]
-
-    return 60_000 / space
-
-
 def apply_effects(audio, bpm, song_effects):
     """
     Processes an MP3 file with a collection of given transformers. Transformers are merely functions that consume a
@@ -69,11 +39,9 @@ def apply_effects(audio, bpm, song_effects):
     :param song_effects: A collection of transformers to apply.
     :return: An AudioSegment that has been processed with the given transformers.
     """
-    beats = list(split_into_beats(audio, 60_000 // bpm))
-    print(f'Processing {len(beats)} beats')
+    beats = list(beats_of(audio, bpm))
 
     for song_effect in song_effects:
         beats = song_effect(beats)
 
-    print('Done.')
     return sum(beats)
