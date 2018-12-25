e825e38eceb1d5da60fb8f8502de8ef938866fa1
dhsavell <dhsavell05@gmail.com>
2018-12-24 19:09:02 -0500

    Auto-generate rough effect documentation

--- tasks.py
+++ tasks.py
@@ -1,6 +1,11 @@
 from invoke import task
 from setuptools import sandbox
 
+# noinspection PyUnresolvedReferences
+from beatmachine.effects import *
+# noinspection PyProtectedMember
+from beatmachine.registry import _registry as registry
+
 
 @task
 def clean(ctx):
@@ -9,3 +14,29 @@ def clean(ctx):
     """
     ctx.run('rm -rf *.mp3')
     sandbox.run_setup('setup.py', ['clean'])
+
+
+@task
+def dump_registered_effects(ctx):
+    with open('EFFECTS.md', 'w') as effects:
+        effects.write("""
+# Effects
+            
+This auto-generated document contains a list of effects defined in the `beatmachine` module available for loading from
+JSON.
+        """.strip())
+        effects.write("\n\n")
+        for effect_name, required_parameters, optional_parameters, factory in sorted(registry.known_effects.values(),
+                                                                              key=lambda l: l.effect_name):
+            effects.write(f"## {effect_name}\n")
+            effects.write(f"{factory.__doc__.strip()}\n\n")
+
+            if required_parameters:
+                effects.write(f"### Required Parameters\n")
+                for parameter_name, parameter_type in required_parameters.items():
+                    effects.write(f"`{parameter_name}`: **{parameter_type.__name__}**\n\n")
+
+            if optional_parameters:
+                effects.write(f"### Optional Parameters\n")
+                for parameter_name, parameter_type in optional_parameters.items():
+                    effects.write(f"`{parameter_name}`: **{parameter_type.__name__}**\n\n")
