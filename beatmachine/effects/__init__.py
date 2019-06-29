77ba68573723c07964df0aea4a8759db78ab157d
dhsavell <dhsavell05@gmail.com>
2019-06-29 09:31:02 -0400

    Add requirements to pyproject.toml

--- beatmachine/effects/__init__.py
+++ beatmachine/effects/__init__.py
@@ -1,3 +1,7 @@
+"""
+The `effects` module provides a base effect class as well as some sample implementations of song effects.
+"""
+
 from . import base, periodic, temporal
 
 load_from_dict = base.EffectRegistry.load_effect_from_dict
