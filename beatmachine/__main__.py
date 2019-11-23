456db9ee93bff75d4fa21c30aa5742e651f5d285
dhsavell <dhsavell05@gmail.com>
2019-11-23 14:56:15 -0500

    Reformat __main__.py

--- beatmachine/__main__.py
+++ beatmachine/__main__.py
@@ -5,10 +5,10 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='beatmachine')
-    parser.add_argument('--input', '-i', help='Input MP3 file', required=True)
-    parser.add_argument('--effects', '-e', help='JSON effects to apply', required=True)
-    parser.add_argument('--output', '-o', help='Output MP3 file', required=True)
+    parser = argparse.ArgumentParser(prog="beatmachine")
+    parser.add_argument("--input", "-i", help="Input MP3 file", required=True)
+    parser.add_argument("--effects", "-e", help="JSON effects to apply", required=True)
+    parser.add_argument("--output", "-o", help="Output MP3 file", required=True)
     args = parser.parse_args()
 
     beats = bm.loader.load_beats_by_signal(args.input)
