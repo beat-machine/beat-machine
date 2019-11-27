400bac7493f0853a71c4991eb563e579443d2c31
dhsavell <dhsavell05@gmail.com>
2019-11-27 17:58:33 -0500

    Re-add dephell and hotfix bad script entry point

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = "dhsavell"
 master_doc = "index"
 
 # The full version, including alpha/beta/rc tags
-release = "2.2.0-alpha.0"
+release = "2.2.0-alpha.1"
 
 # -- General configuration ---------------------------------------------------
 
