b86a3fce700a8a0e354795c824e249dfd81f0db6
dhsavell <dhsavell05@gmail.com>
2020-10-24 13:26:42 -0400

    Reformat again

--- beatmachine/dump_schema.py
+++ beatmachine/dump_schema.py
@@ -2,6 +2,6 @@ import json
 import sys
 from beatmachine.effects.base import EffectRegistry
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(json.dumps(EffectRegistry.dump_list_schema(root=True), indent=2))
     sys.exit(0)
