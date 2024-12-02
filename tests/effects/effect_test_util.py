f0af39d6f02608625ebb92c2f6dee9644110a861
branchpanic <3802908+branchpanic@users.noreply.github.com>
2024-12-02 21:03:12 +0000

    Bump version and reformat

--- tests/effects/effect_test_util.py
+++ tests/effects/effect_test_util.py
@@ -4,5 +4,5 @@ import numpy as np
 def assert_beat_sequences_equal(expected, actual):
     __tracebackhide__ = True
     assert len(expected) == len(actual)
-    for (e, a) in zip(expected, actual):
+    for e, a in zip(expected, actual):
         np.testing.assert_array_equal(e, a)
